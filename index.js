82fd300d1e6c7b3222cb190fe7ab414cc9d6ab85
eugene.timoshenko <da_etimoshenko@phreesia.com>
2025-03-17 11:49:44 +0200

    Added js and db files for contacts cli app

---
+++ index.js
@@ -0,0 +1,43 @@
+import * as contactsService from './contacts.js';
+
+import { program } from "commander";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const contacts = await contactsService.listContacts();
+      console.table(contacts);
+      break;
+
+    case 'get':
+      const contact = await contactsService.getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'add':
+      const newContact = await contactsService.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const removedContact = contactsService.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+
+invokeAction(options);
